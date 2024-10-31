b6932ee49d3689097a93275ec183d8b2de16d9b8
gldtn <g@gldtn.io>
2024-10-31 01:01:18 -0400

    Disable fg

--- lua/themes/cyberdream/cyberdream-colors.lua
+++ lua/themes/cyberdream/cyberdream-colors.lua
@@ -3,7 +3,7 @@ return {
 	-- { label = "bg", color = "#16181a" },
 	{ label = "bgAlt", color = "#1e2124" },
 	{ label = "bgHighlight", color = "#3c4048" },
-	{ label = "fg", color = "#ffffff" },
+	-- { label = "fg", color = "#ffffff" },
 	{ label = "grey", color = "#7b8496" },
 	{ label = "blue", color = "#5ea1ff" },
 	{ label = "green", color = "#5eff6c" },
