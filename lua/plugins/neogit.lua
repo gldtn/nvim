b079e780e944860515a115415c14f0b6161ef432
gldtn <g@gldtn.io>
2024-12-18 15:51:19 -0500

    Changed: commit editor to popup

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -9,16 +9,16 @@ return {
     "sindrets/diffview.nvim", -- diff integration
   },
   cmd = "Neogit",
-        -- stylua: ignore
+    -- stylua: ignore
 		keys = {
 			{ "<c-g>g", "<CMD>Neogit<CR>", desc = "Neogit" },
 			{ "<leader>gl", function() require("neogit").action("log", "log_current", { "--", vim.fn.expand("%") })() end, desc = "Git log for file", },
 			{ "<leader>gr", function()
-                local file = vim.fn.expand("%") vim.cmd([[execute "normal! \<ESC>"]])
-                local line_start = vim.fn.getpos("'<")[2]
-                local line_end = vim.fn.getpos("'>")[2]
-                require("neogit").action("log", "log_current", { "-L" .. line_start .. "," .. line_end .. ":" .. file })()
-            end, desc = "Git log for this range", mode = "v", },
+            local file = vim.fn.expand("%") vim.cmd([[execute "normal! \<ESC>"]])
+            local line_start = vim.fn.getpos("'<")[2]
+            local line_end = vim.fn.getpos("'>")[2]
+            require("neogit").action("log", "log_current", { "-L" .. line_start .. "," .. line_end .. ":" .. file })()
+          end, desc = "Git log for this range", mode = "v", },
 		},
   -- See: https://github.com/NeogitOrg/neogit#configuration
   opts = {
@@ -50,10 +50,12 @@ return {
     sections = {
       rebase = { folded = false },
       recent = { folded = false },
-      untracked = { folded = true, hidden = false },
     },
     kind = "tab", -- tab/floating/split/vsplit
-    commit_editor = { show_staged_diff = false },
+    commit_editor = {
+      kind = "floating",
+      show_staged_diff = false,
+    },
     graph_style = "kitty",
     popup = { kind = "floating" },
     integrations = {
